93da668ab2932b968326007f767a5afb5ed3c102
Florin - Sebastian Serban <serbanf@bzz.ch>
2024-09-30 10:50:19 +0200

    add backend folder add express.js add react env

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path={"/"} exact Component={}></Route>
+        </Routes>
+      </Router>
+  </>);
+}
+
+export default App;
