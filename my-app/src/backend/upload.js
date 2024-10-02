3ebed895c3aca7cc98661092dd1ca473e05d55f0
Florin - Sebastian Serban <serbanf@bzz.ch>
2024-10-02 08:41:14 +0200

    create endpoints create endpoint controller

--- my-app/src/backend/upload.js
+++ my-app/src/backend/upload.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/upload", (req, res) => {
-  console.log(req.body)
-  res.send([[1,2,3,4], [1,2,3,45,6]])
+  console.log(req)
 })
 
 router.get("/upload", (req, res) => {
